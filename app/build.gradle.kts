e519ca612afaa0449bc4d8d6a79ab6bf82c0a7c0
Bobal <swpv@email.cz>
2025-11-24 00:37:58 +0100

    Rebuilding app, fixing splash_screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,7 @@ dependencies {
     implementation(libs.moshi.kotlin)
     implementation(libs.coil.compose)
     implementation(libs.material3)
+    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
